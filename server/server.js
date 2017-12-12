802fe50086118dc790655baa035452a59133a51c
Dustin Phan <phanduyt291@gmail.com>
2017-12-12 01:11:22 -0800

    Supported SSL web server

--- server/server.js
+++ server/server.js
@@ -2,25 +2,31 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+
+var http = require('http');
+var https = require('https');
+var sslConfig = require('./ssl-config.js');
+
+//var loopbackSSL = require('loopback-ssl');
 var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
 
 var app = module.exports = loopback();
 
-var authCheck = jwt({
-  secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        // YOUR-AUTH0-DOMAIN name e.g https://prosper.auth0.com
-        jwksUri: "https://bec-authen-demo.auth0.com/.well-known/jwks.json"
-    }),
-    // This is the identifier we set when we created the API
-    audience: '{http://192.168.1.122:3000/api}',
-	//audience: 'https://bec-authen-demo.auth0.com/api/v2/',
-    issuer: 'https://bec-authen-demo.auth0.com/',
-    algorithms: ['RS256']
-});
+//var authCheck = jwt({
+//  secret: jwks.expressJwtSecret({
+//        cache: true,
+//        rateLimit: true,
+//        jwksRequestsPerMinute: 5,
+//        // YOUR-AUTH0-DOMAIN name e.g https://prosper.auth0.com
+//        jwksUri: "https://bec-authen-demo.auth0.com/.well-known/jwks.json"
+//    }),
+//    // This is the identifier we set when we created the API
+//    audience: '{http://192.168.1.122:3000/api}',
+//	//audience: 'https://bec-authen-demo.auth0.com/api/v2/',
+//    issuer: 'https://bec-authen-demo.auth0.com/',
+//    algorithms: ['RS256']
+//});
 
 //app.use(authCheck);
 
@@ -32,25 +38,51 @@ var authCheck = jwt({
 //    res.json("It has valid token", req.user);
 //});
 
-app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).send('Invalid token, or no token supplied!');
-    } else {
-        res.status(401).send(err);
-    }
-});
+//app.use(function (err, req, res, next) {
+//    if (err.name === 'UnauthorizedError') {
+//        res.status(401).send('Invalid token, or no token supplied!');
+//    } else {
+//        res.status(401).send(err);
+//    }
+//});
 
-app.start = function() {
+app.start = function(httpOnly) {
+	if (httpOnly === undefined) {
+		httpOnly = process.env.HTTP;
+	}
+	var server = null;
+	if (!httpOnly) {
+		var options = {
+			key: sslConfig.privateKey,
+			cert: sslConfig.certificate,
+		};
+		server = https.createServer(options, app);
+	} else {
+		server = http.createServer(app);
+	}
+
+	server.listen(app.get('port'), function() {
+		var baseUrl =(httpOnly ? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
+		app.emit('started', baseUrl);
+		console.log('Web server listening at: %s%s', baseUrl, '/');
+		if (app.get('loopback-component-explorer')) {
+			var explorerPath = app.get('loopback-component-explorer').mountPath;
+			console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+		}
+	});
+
+	return server;
+
   // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+  //return app.listen(function() {
+  //  app.emit('started');
+  //  var baseUrl = app.get('url').replace(/\/$/, '');
+  //  console.log('Web server listening at: %s', baseUrl);
+  //  if (app.get('loopback-component-explorer')) {
+  //    var explorerPath = app.get('loopback-component-explorer').mountPath;
+  //    console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+  //  }
+  //});
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
@@ -58,7 +90,8 @@ app.start = function() {
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
-  // start the server if `$ node server.js`
+	//start the server if `$ node server.js`
   if (require.main === module)
     app.start();
 });
+//return loopbackSSL.startServer(app);
